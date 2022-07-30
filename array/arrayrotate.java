0a9803ba81a6c8b870cf3a84c401e20190d6e551
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-30 22:14:10 +0530

    rotate array in right side k times

--- array/arrayrotate.java
+++ array/arrayrotate.java
@@ -2,16 +2,19 @@ public class arrayrotate {
     public static void main(String[] args) {
         int[] nums={3,5,7,9};
         int k=2;
+        // for how many times k will be sift.
         for(int i=0;i<=k;i++){
-            leftswap(nums);
+            System.out.println(leftswap(nums));
         }
     }
 
-     static void leftswap(int[] nums) {
+     static int leftswap(int[] nums) {
         int temp=nums[0];
+        //for moving 5,7,9 in left side.
         for(int i=0;i<nums.length-1;i++){
-            nums[1]=nums[i+1];
+            nums[i]=nums[i+1];
         }
         nums[nums.length-1]=temp;
+        return nums[nums.length-1];
     }
 }
