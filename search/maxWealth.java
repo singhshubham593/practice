e1fd81f33c6cbf34576e169d32b785e73cf07291
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-08-27 10:44:56 +0530

    java:1672 Richest customer wealth(leetcode).

--- search/maxWealth.java
+++ search/maxWealth.java
@@ -9,16 +9,23 @@ public class maxWealth {
         int [][] acounts = {{1,5},
                             {7,3},
                             {3,5} };
-        System.out.println(maximumWealth(acounts));
+        System.out.print("THe richest person is ");
+        //function call and print
+        System.out.print(maximumWealth(acounts));
     }
     static int maximumWealth(int[][] acounts){
+        // person =row
+        //acount =col
         int ans = Integer.MIN_VALUE;
         for(int person = 0;person<acounts.length;person++){
+            //when you start a new col, take a new sum for that row
             int sum =0;
             for(int acount = 0;acount<acounts[person].length;acount++){
                 sum +=acounts[person][acount];
 
             }
+            //now, we have to sum of acount of person
+            //check with overall ans
             if(sum>ans){
                 ans =sum;
             }
