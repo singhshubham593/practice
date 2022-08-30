333800f71aee8b0d823e087c63359e03c3304aa7
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-08-30 22:34:53 +0530

    java: Bubble sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -8,9 +8,13 @@ public class BubbleSort {
     }
 
     static void bubble(int[] arr){
+        // run the steps n-1 times
         for(int i=0;i< arr.length;i++){
+            // for each step, max item will come at the last respective index
             for(int j=1;j < arr.length-i;j++){
+                // swap if the item is smaller than the previous item
                 if(arr[j] <arr[j-1]){
+                    //swap
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] =temp;
