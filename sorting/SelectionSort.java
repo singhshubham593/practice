21698f6186d007ca7550b053455fed7de7e22d51
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-08-31 13:27:53 +0530

    java: Seletion sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-
+//Selection Sort
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {4,2,96,7,8,3};
@@ -7,17 +7,23 @@ public class SelectionSort {
         System.out.println(Arrays.toString(arr));
     }
     static void selectionSort(int [] arr){
+        // for placing maximum number in last
         for(int i = 0;i< arr.length;i++){
+            //asign value in last
             int last = arr.length -1-i;
+            // funcyion call and store value
             int maxIndex = getMaxIndex(arr,0,last);
+            // funcyion call and store value
             swap(arr,maxIndex,last);
         }
     }
+    //for swap using third variable
     static void swap(int[] arr,int first,int second){
         int temp = arr[first];
         arr[first]=arr[second];
         arr[second]=arr[temp];
     }
+    // find max element
     static int getMaxIndex(int [] arr,int start,int end){
         int max =start;
         for(int i =start;i<= end;i++){
