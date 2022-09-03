7ea939ee2d414c78caecbac383cae5c42bc4b6ff
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-09-03 16:39:28 +0530

    java: Bubbe sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -8,8 +8,10 @@ public class BubbleSort {
     }
 
     static void bubble(int[] arr){
+        boolean swapped;
         // run the steps n-1 times
         for(int i=0;i< arr.length;i++){
+            swapped = false;
             // for each step, max item will come at the last respective index
             for(int j=1;j < arr.length-i;j++){
                 // swap if the item is smaller than the previous item
@@ -18,7 +20,12 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] =temp;
+                    swapped = true;
                 }
+            }
+            // if you did not swap for a particular value of i, it means the array is sorted heace stop the program
+            if(swapped == false){
+                break;
             }
         }
     }
