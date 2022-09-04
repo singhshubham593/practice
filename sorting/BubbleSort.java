977f5e1f6f6c9ea78142d3108cb930188954bafd
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-09-04 13:20:20 +0530

    java: Bubble sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 // Bubble Sort example
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr ={2,4,8,6,7,9};
+        int[] arr ={2,4,8,6,7,3,9};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
