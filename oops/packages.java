0fbeb1d43c9b37bf286871256c3d0c0490992b16
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-08 21:19:05 +0530

    java: OOPS : package.

--- oops/packages.java
+++ oops/packages.java
@@ -1,8 +1,15 @@
 // Build-in package.
-import java.util.Scanner; //import package.name.Class;   // Import a single class
-                          //import package.name.*;   // Import the whole package
+import java.util.Scanner;
+//import package.name.Class;   // Import a single class
+//import package.name.*;   // Import the whole package
+
+// package are divided into two categories.
+// 1.built in package
+//2. user defined package
+
 
 class packages {
+    // 1. build in package
     // import a class
 
    // Scanner class to get user input:
@@ -21,7 +28,7 @@ package mypack; // mypack is user define package.
 
 class MyPackageClass {
     public static void main(String[] args) {
-        System.out.println("This is my package!");
+        System.out.println("This is User defined package!");
     }
 }
  */
