f5eda89f3f9a5ec6351cee04e28e01632dc22932
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-12 15:19:47 +0530

    java: pattern: pattern question no 1.

---
+++ patterns/patternQuestions/pattern1.java
@@ -0,0 +1,14 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        pattern1(5);
+    }
+
+    static void pattern1(int n){
+        for ( int row = 1; row <= n ; row++){
+            for ( int col = 1; col <= row ; col++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
