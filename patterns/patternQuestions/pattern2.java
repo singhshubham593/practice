deda3841b0f45252fdba69ac9bce853a035bc887
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-15 20:35:00 +0530

    java: pattern: pattern Question number 2.

--- patterns/patternQuestions/pattern2.java
+++ patterns/patternQuestions/pattern2.java
@@ -13,4 +13,21 @@
  */
 
 public class pattern2 {
+    public static void main(String[] args) {
+        //function ca//
+        pattern1(5);
+    }
+
+    static void pattern1(int n){
+        //for row
+        for ( int row = 1; row <= n ; row++){
+            // for every row,run the coloum
+            for ( int col = 1; col <= 4 ; col++){
+                // print Star
+                System.out.print("* ");
+            }
+            // for next line after coloum loop is end
+            System.out.println("");
+        }
+    }
 }
