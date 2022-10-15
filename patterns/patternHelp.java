05187431ae5b30b4f2611672218b3280f9cf9f18
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-15 20:29:32 +0530

    java: pattern: Stepwise process to solve pattern Question.

---
+++ patterns/patternHelp.java
@@ -0,0 +1,34 @@
+// Question
+/*
+     COLOUM
+     _ _ _ _ _
+   | *
+ R | * *
+ O | * * *
+ W | * * * *
+   | * * * * *
+
+ */
+
+
+// How to Solve all question of pattern
+
+//step no1).no of lines = no of rows = no of times outer loop will run.
+
+  /* for(int row = 1; row <=n ; row++){
+
+           }
+   */
+
+// step no2). Identify for every row no ,
+
+// 1.how many coloum are there
+// 2.types of element in coloum
+
+/*
+ for(int col = 1; col <= row ; col++){
+       System.out.print("* ");
+        }
+ */
+
+// step no 3). what do you need to print
