3d80df886eb0f8362507b771d380687afde364fe
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-25 21:57:12 +0530

    java: String : Different case in String.

--- String/CompareString.java
+++ String/CompareString.java
@@ -1,2 +1,16 @@
 public class CompareString {
+    public static void main(String[] args) {
+
+  // variable is different for same value.but,result is true.
+
+        String a ="Shubham";
+        String b = "Shubham";
+        System.out.println(a==b);
+
+ // creating different object for same value.but,result is false.
+
+        String name1 = new String("shubham");
+        String name2 = new String("shubham");
+        System.out.println(name1==name2);
+    }
 }
