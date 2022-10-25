13841d3448a7bbae8814063119eb4c21d2f5fe56
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-25 21:55:49 +0530

    java: String : Introduction to String.

--- String/Str.java
+++ String/Str.java
@@ -1,7 +1,7 @@
 public class Str {
     public static void main(String[] args) {
         //concatenation
-        String firstFame = "Shubham";
+        String firstFame = "Shubham"; // dataType refferenceVariable = "object";
         String lastName = "Singh";
         String fullName = firstFame + " " + lastName; // addition of two string
         System.out.println(fullName);
