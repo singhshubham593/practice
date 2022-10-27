78d47628aaeaa93c5691967f67dad8bbc22c7edf
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-10-27 13:16:45 +0530

    java: String : different type of method and uses in String.

---
+++ String/StringMethod.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class StringMethod {
+    public static void main(String[] args) {
+        String name="Shubham Singh";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        //name.toLowerCase();
+        //name.indexOf();
+        //And many more;
+    }
+}
