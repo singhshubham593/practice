b4bd4d1444d1c9b4f847e0aed29c9c9479841d63
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-01-01 09:28:20 +0530

    java : sort: code and concept of merge sort.                                                  }.

--- sorting/mergeSort.java
+++ sorting/mergeSort.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
-
+// Code and concept of merge sort
 public class mergeSort {
     // mergeSort function that takes in an array of integers and sorts it in ascending order using the merge sort algorithm.
-    //
+
     public static void mergeSort(int[] array) {
          if (array.length > 1) {
+             // function call and initialise
              int[] left = leftHalf(array);
              int[] right = rightHalf(array);
 
@@ -16,6 +17,7 @@ public class mergeSort {
     }
     //The leftHalf and rightHalf functions are helper functions that split the input array into its left and right halves,
     public static int[] leftHalf(int[] array) {
+        //devide array value and shifting
         int size1 = array.length / 2;
         int[] left = new int[size1];
         for (int i = 0; i < size1; i++) {
@@ -25,6 +27,7 @@ public class mergeSort {
     }
 
     public static int[] rightHalf(int[] array) {
+        //devide array value and shifting
         int size1 = array.length / 2;
         int size2 = array.length - size1;
         int[] right = new int[size2];
@@ -33,6 +36,7 @@ public class mergeSort {
         }
         return right;
     }
+    //conquer the divided value from left and right in sorted manner
     public static void merge(int[] result, int[] left, int[] right) {
         int i1 = 0;
         int i2 = 0;
@@ -47,7 +51,7 @@ public class mergeSort {
             }
         }
     }
-
+    //main function for showing array before and after sorting array.
     public static void main(String[] args) {
         int[] array = {5, 2, 4, 7, 1, 3, 2, 6};
 
