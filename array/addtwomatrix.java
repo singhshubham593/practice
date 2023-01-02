a600f86047210f1cf7fff4f69120378f38e5df8e
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-01-02 22:26:19 +0530

    java : array: Addition of two matrix.

--- array/addtwomatrix.java
+++ array/addtwomatrix.java
@@ -21,6 +21,7 @@ public class addtwomatrix {
             for (d = 0; d < n; d++)
                 first[c][d] = in.nextInt();
 
+
         System.out.println("Enter the elements of second matrix::");
         //for element in second matrix
         for (c = 0; c < m; c++)
@@ -32,14 +33,15 @@ public class addtwomatrix {
             for (d = 0; d < n; d++)
                 sum[c][d] = first[c][d] + second[c][d];  //replace '+' with '-' to subtract matrices
 
+
         System.out.println("Sum of entered matrices::");
 
         // printing sum of matrix
-        for (c = 0; c < m; c++) {
-            for (d = 0; d < n; d++)
-                System.out.print(sum[c][d] + "\t");
+        for (c = 0; c < m; c++){
+           for (d = 0; d < n; d++)
+               System.out.print(sum[c][d] + "\t");
 
-            System.out.println();
+           System.out.println();
         }
     }
 }
