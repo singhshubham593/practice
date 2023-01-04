0de73bbdf74863ca0d6cd557ac287bf89f0106d0
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-01-04 05:40:54 +0530

    java : Concept Of Recursion.

--- recursion/conceptOfRecursion.java
+++ recursion/conceptOfRecursion.java
@@ -1,2 +1,38 @@
 public class conceptOfRecursion {
+    /*
+
+    public static void main(String[] args) {
+        //write a function that take in a number and print it
+        //print first 4 number : 1234
+        print1(1);
+    }
+    //functjion call another function
+    //body and definition are same,but only name is different.
+    //while the function is not finished executing,it will remain in stack.
+    //while the function is finished executing,it is remove from stack and the flow of program is restored to where the function is called.
+    static void print1(int n){
+        System.out.println(n);
+        print2(2);
+    }
+    static void print2(int n){
+        System.out.println(n);
+        print3(3);
+    }
+    static void print3(int n) {
+        System.out.println(n);
+        print4(4);
+    }
+    static void print4(int n){
+        System.out.println(n);
+    }
+    */
+    public static void main(String[] args) {
+        //write a function that take in a number and print it
+        //print first 4 number : 1234
+        print1(1);
+    }
+    static void print1(int n){
+        System.out.println(n);
+
+    }
 }
