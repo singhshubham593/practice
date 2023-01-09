7616d60b6ea9ecbbdfff4f50583af15e9a3245d7
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-01-09 20:46:41 +0530

    java :Recursion:find fibonacci number.

--- recursion/fibonacciNo.java
+++ recursion/fibonacciNo.java
@@ -1,10 +1,19 @@
 public class fibonacciNo {
     //finding fibonacci number -->0,1,1,2,3,5,8,13,21....
     public static void main(String[] args) {
+        /*
         //fun initialise a value
         int ans=fibo(7);
         //fun call as value initialise
         System.out.println(ans);
+
+        */
+        for(int i=0;i<11;i++){
+            System.out.println(fiboFormula(i));
+        }
+    }
+    static int fiboFormula(int n){
+        return (int)(Math.pow(((1+Math.sqrt(5))/2),n)/Math.sqrt(5));
     }
     // return type is Integer
     static int fibo(int n){
