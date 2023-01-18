92ec44db908ad47932d2f08b91ce72f30bd7aaaf
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-01-18 11:05:50 +0530

    java :pattern: pattern3     COLOUM     ******   R *****   O ****   W ***     **     *     .

--- patterns/patternQuestions/pattern3.java
+++ patterns/patternQuestions/pattern3.java
@@ -9,5 +9,16 @@
     *
  */
 public class pattern3 {
-     
+    public static void main(String[] args) {
+        pattern(6);
+    }
+
+    static void pattern(int n){
+        for(int row=1;row<=n;row++){
+            for(int coloum=1;coloum<=n-row+1;coloum++){
+                System.out.println("* ");
+            }
+            System.out.println();
+        }
+    }
 }
