0265f9d74a9e756e9ec66a73bdcb608f649835f0
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-07-12 05:35:36 +0530

    java :pattern: pattern4
           *
          * *
         * * *
        * * * *
       * * * * *

--- patterns/patternQuestions/pattern4.java
+++ patterns/patternQuestions/pattern4.java
@@ -1,29 +1,33 @@
+
+
 // pattern question
 /*
-     *
-    * *
-   * * *
-  * * * *
- * * * * *
+      COLOUM
+      _ _ _ _ _
+   |      *
+ R |     * *
+ O |    * * *
+ W |   * * * *
+   |  * * * * *
+
  */
 public class pattern4 {
     public static void pyramidPattern(int n) {
         int i;
-        for (i = 0; i < n; i++) //outer loop for number of rows(n) { for (int j=n-i; j>1; j--) //inner loop for spaces
+        for (i = 1; i < n; i++) //outer loop for number of rows(n) { for (int j=n-i; j>1; j--) //inner loop for spaces
         {
-            System.out.print(" "); //print space
+            for(int k=n;k>i;k--){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) //inner loop for number of columns
+            {
+                System.out.print("* "); //print star
+            }
+            System.out.println(" "); //print space
         }
-        for (int j = 0; j <= i; j++) //inner loop for number of columns
-        {
-            System.out.print("* "); //print star
-        }
-
-        System.out.println(); //ending line after each row
+    }
+    public static void main(String argu[]){
+        pyramidPattern(6);
     }
 
-    public static void main(String args[]) //driver function
-    {
-        int n = 5;
-        pyramidPattern(n);
-    }
 }
