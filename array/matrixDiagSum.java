7c1fd1762475ff605b77738fc95960a2c6035c07
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-07-27 22:10:33 +0530

    java :Array:  sum of diagonal of 2d matrix

---
+++ array/matrixDiagSum.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+// matrix diagonal sum
+public class matrixDiagSum {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},
+                    {4,5,6},
+                    {7,8,9},
+        };
+        System.out.println(Arrays.toString(arr));
+    }
+    static void diaSum(int[][] arr){
+        int sum=0;
+        for(int i=0;i<= arr.length;i++){
+
+        }
+    }
+}
