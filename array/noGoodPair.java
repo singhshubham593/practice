35c262620b9c2a565452aa800295b73c72e1a399
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-10-07 16:45:21 +0530

    leetcode : 1512.Number of Good Pair

--- array/noGoodPair.java
+++ array/noGoodPair.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 1512.Number of Good Pair
 
@@ -16,4 +18,16 @@ Output: 6
 Explanation: Each pair in the array are good.
  */
 public class noGoodPair {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,1,1,3};
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j< arr.length;j++){
+                if(arr[i]==arr[j]){
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
 }
