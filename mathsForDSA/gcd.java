7cb00521e711761919ce264f7a6fced324ebf9a0
Shubham singh <rajputshubhamsingh593@gmail.com>
2023-12-31 14:36:39 +0530

    gcd with different approach

---
+++ mathsForDSA/gcd.java
@@ -0,0 +1,24 @@
+// gcd
+public class gcd {
+    public static void main(String[] args) {
+        /*
+        int n1=9,n2=12;
+        int gcds = 1;
+        for (int i=1;i<=Math.min(n1,n2);i++){
+            if(n1%i==0 && n2%i==0){
+                gcds=i;
+            }
+        }
+        System.out.println(gcds);
+         */
+        /*
+        int n1=9,n2=12;
+        for (int i=Math.min(n1,n2);i>=1;i--){
+            if(n1%i==0 && n2%i==0){
+                System.out.println(i);
+                break;
+            }
+        }
+         */
+    }
+}
