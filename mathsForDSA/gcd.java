0a4d0f07b4ba90fbb1414fa5d5f841ff630c0124
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-01-01 12:56:47 +0530

    find gcd using various meethod

--- mathsForDSA/gcd.java
+++ mathsForDSA/gcd.java
@@ -1,24 +1,43 @@
 // gcd
 public class gcd {
     public static void main(String[] args) {
+        int a=52,b=10;
         /*
-        int n1=9,n2=12;
+        // first approach
         int gcds = 1;
-        for (int i=1;i<=Math.min(n1,n2);i++){
-            if(n1%i==0 && n2%i==0){
+        for (int i=1;i<=Math.min(a,b);i++){
+            if(a%i==0 && b%i==0){
                 gcds=i;
             }
         }
         System.out.println(gcds);
          */
         /*
-        int n1=9,n2=12;
-        for (int i=Math.min(n1,n2);i>=1;i--){
-            if(n1%i==0 && n2%i==0){
+        // 2nd approach
+        for (int i=Math.min(a,b);i>=1;i--){
+            if(a%i==0 && b%i==0){
                 System.out.println(i);
                 break;
             }
         }
          */
+        // using eucliden Algorithm
+        /*
+         a>b
+         gcd(a,b)==gcd(a%b,b)
+           (greater%smaller)->one of them is zero,the other is gcd
+         */
+        while(a>0&&b>0){
+            if(a>b){
+                a=a%b;
+            }else {
+                b=b%a;
+            }
+        }
+        if(a==0){
+            System.out.println(b);
+        }else {
+            System.out.println(a);
+        }
     }
 }
