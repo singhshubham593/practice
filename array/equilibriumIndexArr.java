40d164f0c6897034d7885cc78972b9eea027d1db
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-01-01 12:56:47 +0530

    find equilibrium Index using array

--- array/equilibriumIndexArr.java
+++ array/equilibriumIndexArr.java
@@ -8,10 +8,27 @@ output = 2
 */
 public class equilibriumIndexArr {
     public static void main(String[] args) {
-        int arr[]={-3,5,1,-4,4,2};
+        int arr[]={-7,1,5,2,-4,3,0};
         System.out.print(" the equilibrium index is : ");
         System.out.println(equilibrium(arr));
     }
+    // optimise approach
+    static int equilibrium(int[] arr){
+        int sum=0,leftsum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        for(int i=0;i<arr.length;i++){
+            sum=sum-arr[i];
+            if(leftsum==sum){
+                return i;
+            }
+            leftsum=leftsum+arr[i];
+        }
+        return -1;
+    }
+    /*
+    // simple approach
     static int equilibrium(int arr[]){
         int i;
         for(i=0;i< arr.length-1;i++){
@@ -29,4 +46,5 @@ public class equilibriumIndexArr {
         }
         return i;
     }
+     */
 }
