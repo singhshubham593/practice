4755ac6bcaa4d0ad3a471cbdb80c6cef68c5adbb
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-02-22 20:34:30 +0530

    to rotate an array k times

--- array/rotateArrbyK.java
+++ array/rotateArrbyK.java
@@ -9,9 +9,9 @@ public class rotateArrbyK {
     // rotate function
     static int[] rotate(int[] arr, int k){
        // k%=arr.length;
-        reverce(arr,0, arr.length-1);
-        reverce(arr,0,k-1);
-        reverce(arr,k, arr.length-1);
+        reverce(arr,0, arr.length-1); // reverse array 0 to n-1
+        reverce(arr,0,k-1);           // reverse array 0 to k-1
+        reverce(arr,k, arr.length-1);      // reverse array k to n-1
         return arr;
     }
     // reverce function
@@ -26,4 +26,23 @@ public class rotateArrbyK {
             end--;
         }
     }
+    /*
+     public static void main(String[] args) {
+        int arr[]={1,2,3,4,5}; //   3,4,5,1,2
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(rotat(arr,3)));
+    }
+     static int[] rotat(int[] arr, int k){
+        for(int i=0;i<k;i++){
+            reverse(arr);
+        }
+        return arr;
+    }
+    static void reverse(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+     */
 }
